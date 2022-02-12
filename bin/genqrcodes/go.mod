012c00900cc70ee7166a6d2043694079c4e4c88f
crypticplank <bplank03@gmail.com>
2022-02-12 13:41:03 -0500

    this was very painful

--- bin/genqrcodes/go.mod
+++ bin/genqrcodes/go.mod
@@ -2,4 +2,7 @@ module brandonplank.org/genqrcodes
 
 go 1.17
 
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+require (
+	github.com/ledongthuc/pdf v0.0.0-20210621053716-e28cb8259002
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+)
--- bin/genqrcodes/main.go
+++ bin/genqrcodes/main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/ledongthuc/pdf"
 	"github.com/skip2/go-qrcode"
 	"log"
 	"os"
+	"regexp"
 )
 
 const outputPath = "codes"
+
+func readPdf(path string) (string, error) {
+	var ret string
+	f, r, err := pdf.Open(path)
+	defer func() {
+		_ = f.Close()
+	}()
+	if err != nil {
+		return "", err
+	}
+	totalPage := r.NumPage()
+
+	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
+		p := r.Page(pageIndex)
+		if p.V.IsNull() {
+			continue
+		}
+
+		rows, _ := p.GetTextByRow()
+		for _, row := range rows {
+			for _, word := range row.Content {
+				ret += word.S + "\n"
+			}
+		}
+	}
+	return ret, nil
+}
 
 func main() {
 	if _, err := os.Stat(outputPath); os.IsNotExist(err) {
 		_ = os.Mkdir(outputPath, os.ModePerm)
 	}
-	students, err := os.Open("students.txt")
+
+	content, err := readPdf("report.pdf")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fileScanner := bufio.NewScanner(students)
 
-	fileScanner.Split(bufio.ScanLines)
+	// this was painful
+	var re = regexp.MustCompile(`(?m)^([a-zA-Z\-]+)\s*,\s*([a-zA-Z]+)(\s+([a-zA-Z]+))?$`)
 
-	for fileScanner.Scan() {
-		log.Println(fileScanner.Text())
-		err := qrcode.WriteFile(fileScanner.Text(), qrcode.Medium, 256, fmt.Sprintf("%s/%s-code.png", outputPath, fileScanner.Text()))
+	for _, match := range re.FindAllString(content, -1) {
+		err = qrcode.WriteFile(match, qrcode.Medium, 256, fmt.Sprintf("%s/%s-code.png", outputPath, match))
 		if err != nil {
 			fmt.Printf("Couldn't create qrcode:,%v", err)
 		}
 	}
-
-	students.Close()
 }
